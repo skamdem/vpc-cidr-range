0af6d08ae30d5f1bf3a643f7d48ca3b63925bab4
Sylvain K <sylvain.kamdem@gmail.com>
2023-07-04 12:04:05 +0100

    added lock file and "region" variable

--- variables.tf
+++ variables.tf
@@ -1,3 +1,9 @@
+# General variables
+variable "region" {
+  type        = string
+  description = "Default region for aws provider"
+}
+
 variable "cidr_block" {
   type        = string
   description = "cidr_block of the vpc in this example"
